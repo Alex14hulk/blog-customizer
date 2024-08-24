dce53acc67ca144a7b4943064e1229a25a5d0d6e
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2024-08-24 13:52:25 +0500

    final_2

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,25 +1,36 @@
 import arrow from 'src/images/arrow.svg';
 import React from 'react';
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
 interface ArrowButtonProps {
-	onClick?: OnClick;
-	isOpenMenu?: boolean;
+	onClick: OnClick;
+	isOpenMenu: boolean;
 }
 
-export const ArrowButton = (props: ArrowButtonProps) => {
+export const ArrowButton = (props: ArrowButtonProps): JSX.Element => {
+	const {onClick, isOpenMenu} = props;
+
+	const arrowButtonContainer = clsx(styles.container, {
+		[styles.container_open]: isOpenMenu,
+	});
+
+	const arrowImagine = clsx(styles.arrow, {
+		[styles.arrow_open]: isOpenMenu,
+	});
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={`${styles.container} ${props.isOpenMenu ? styles.container_open : ""}`} 
+			className={arrowButtonContainer} 
 			onClick={props.onClick}>
-			<img src={arrow} alt='иконка стрелочки' className={`${styles.arrow} ${props.isOpenMenu ? styles.arrow_open : ""}`} />
+			<img src={arrow} alt='иконка стрелочки' className={arrowImagine} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -6,20 +6,20 @@ import { Text } from '../text';
 import { RadioGroup } from '../radio-group';
 import { Separator } from '../separator';
 import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
-import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState,
+import { ArticleStateType, OptionType, backgroundColors, contentWidthArr, defaultArticleState,
 		 fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 interface ArticleParamsFormProps {
-	articleState: ArticleStateType;
 	applyArticleState: (state: ArticleStateType) => void;
 }
 
-export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+export const ArticleParamsForm = ( {applyArticleState}: ArticleParamsFormProps) => {
 	const rootRef = useRef<HTMLDivElement | null>(null);
 	const [isOpenMenu, setIsOpenMenu] = useState(false);
 	const onClickIsOpenMenu = () => {
-		setIsOpenMenu(isOpenMenu === false ? true : false);
+		setIsOpenMenu(!isOpenMenu);
 	}
 
 	useOutsideClickClose({
@@ -29,63 +29,56 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 		onChange: setIsOpenMenu,
 	});
 
-	const [formState, setFormState] = useState({
-		backgroundColor: props.articleState.backgroundColor,
-		contentWidth: props.articleState.contentWidth,
-		fontColor: props.articleState.fontColor,
-		fontFamilyOption: props.articleState.fontFamilyOption,
-		fontSizeOption: props.articleState.fontSizeOption,
+	const [formState, setFormState] = useState<ArticleStateType>(() => {
+		const newFormState = localStorage.getItem('formState');
+		return newFormState ? JSON.parse(newFormState) : defaultArticleState;
 	});
 
 	const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>):void => {
 		event.preventDefault();
-		props.applyArticleState({
-			backgroundColor: formState.backgroundColor,
-			contentWidth: formState.contentWidth,
-			fontColor: formState.fontColor,
-			fontFamilyOption: formState.fontFamilyOption,
-			fontSizeOption:formState.fontSizeOption,
-		})
-		setIsOpenMenu(false)
+		applyArticleState(formState);
+		setIsOpenMenu(false);
 	}
+
+	const changeFormSubmit = (key: keyof ArticleStateType, option: OptionType) => {
+		setFormState((prevState: ArticleStateType) => ({
+			...prevState,
+			[key]: option,
+		})); 
+	};
 
 	const resetFormSubmit = (event: React.FormEvent<HTMLFormElement>):void => {
 		event.preventDefault();
-		setFormState(defaultArticleState)
-		props.applyArticleState(defaultArticleState);
+		setFormState(defaultArticleState);
+		applyArticleState(defaultArticleState);
 	}
 
 	return (
 		<>
 			<ArrowButton onClick={onClickIsOpenMenu} isOpenMenu={isOpenMenu}/>
-			<aside className={`${styles.container} ${isOpenMenu ? styles.container_open : ""}`} ref={rootRef}>
+			<aside className={clsx(styles.container, {[styles.container_open]: isOpenMenu })} ref={rootRef}>
 				<form className={styles.form} onSubmit={handleFormSubmit} onReset={resetFormSubmit}>
 					<Text as={'h2'} uppercase={true} weight={800} size={31}>
 						Задайте параметры</Text>
 					<Select selected={formState.fontFamilyOption} options={fontFamilyOptions} 
-							onChange={(selected) => { 
-								setFormState((prevState) => ({ ...prevState, fontFamilyOption: selected})) }} 
+							onChange={(option) => changeFormSubmit('fontFamilyOption', option)} 
 							title="Шрифт">
 					</Select>
 					<RadioGroup selected={formState.fontSizeOption} options={fontSizeOptions} 
-								onChange={(selected) => { 
-									setFormState((prevState) => ({ ...prevState, fontSizeOption: selected})) }} 
+								onChange={(option) => changeFormSubmit('fontSizeOption', option)}
 								title='Размер шрифта' name="Размер шрифта">
 					</RadioGroup>
 					<Select selected={formState.fontColor} options={fontColors} 
-							onChange={(selected) => { 
-								setFormState((prevState) => ({ ...prevState, fontColor: selected})) }} 
+							onChange={(option) => changeFormSubmit('fontColor', option)}
 							title="Цвет шрифта">
 					</Select>
 					<Separator/>
 					<Select selected={formState.backgroundColor} options={backgroundColors} 
-							onChange={(selected) => { 
-								setFormState((prevState) => ({ ...prevState, backgroundColor: selected})) }} 
+							onChange={(option) => changeFormSubmit('backgroundColor', option)}
 							title="Цвет фона">
 					</Select>
 					<Select selected={formState.contentWidth} options={contentWidthArr} 
-							onChange={(selected) => { 
-								setFormState((prevState) => ({ ...prevState, contentWidth: selected})) }} 
+							onChange={(option) => changeFormSubmit('contentWidth', option)}
 							title="Ширина контента">
 					</Select>
 					<div className={styles.bottomContainer}>
